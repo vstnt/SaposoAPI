d26d8f28f5735aa3d2e9510e08d5a8b8a0401fa9
vstnt <r.rms087@gmail.com>
2024-07-30 16:51:58 -0300

    Corrigir registro de ts-node no ace.js

--- ace.js
+++ ace.js
@@ -19,8 +19,15 @@
 /**
  * Register hook to process TypeScript files using ts-node
  */
-import { register } from 'node:module'
-register('ts-node/esm', import.meta.url)
+//import { register } from 'node:module'
+//register('ts-node/esm', import.meta.url)
+import 'ts-node/register';
+
+/*A abordagem anterior está tentando usar o módulo nativo node:module para registrar
+ o ts-node/esm. O problema aqui é que a API de módulos do Node.js pode não estar
+  configurada da forma esperada para lidar com esse tipo de registro, especialmente
+   no contexto de um ambiente de build como o Render. Assim, trocamos ela por sugestão do gpt para
+   tentar resolver o erro de módulo não encontrado no deploy do Render.*/
 
 /**
  * Import ace console entrypoint
